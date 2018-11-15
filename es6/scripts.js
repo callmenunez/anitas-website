d5e38209bd213114c3845391274319f33672c1f3
David Nunez <david.nunez@yodle.com>
2018-11-15 00:33:23 -0500

    initialized masonry

--- es6/scripts.js
+++ es6/scripts.js
@@ -1,18 +1,26 @@
 "use strict" ;
 console.log("its working, yay");
-var elem = document.querySelector('.grid');
-var msnry = new Masonry( elem, {
-  // options
-  itemSelector: '.grid-item',
-  columnWidth: 200
+
+
+$(window).on('load', function(){
+  $('.grid').masonry({
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-item',
+    percentPosition: true
+  });
 });
 
-// element argument can be a selector string
-//   for an individual element
-var msnry = new Masonry( '.grid', {
-  // options
-  itemSelector: '.grid-item',
-  // use element for option
-  columnWidth: '.grid-sizer',
-  percentPosition: true
-});
+
+// var elem = document.querySelector('.grid');
+// var msnry = new Masonry( elem, {
+//   // options
+//   itemSelector: '.grid-item',
+//   columnWidth: '.grid-item'
+// });
+
+// // element argument can be a selector string
+// //   for an individual element
+// var msnry = new Masonry( '.grid', {
+//   // options
+// });
