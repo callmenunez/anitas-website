c9bf382ba29d7371c81f5ed3f9e1caf0ffeba308
David Nunez <david.nunez@yodle.com>
2018-12-07 17:58:59 -0500

    updated index page

--- es6/scripts.js
+++ es6/scripts.js
@@ -2,15 +2,30 @@
 console.log("its working, yay");
 
 // init Masonry
-var elem = document.querySelector('.grid');
 var $grid = $('.grid').masonry({
   itemSelector: '.grid-item',
   columnWidth: '.grid-sizer',
   gutter: '.gutter-sizer',
-  // resize: ,
   percentPosition: true
 });
 // layout Masonry after each image loads
 $grid.imagesLoaded().progress( function() {
   $grid.masonry('layout');
 });  
+
+VanillaTilt.init(document.querySelector(".tilting-logo"), {
+  max: 10,
+  speed: 400
+});
+
+// function toggle(){
+//   var off=document.getElementById('splash');
+//   if (off.style.display == "none") {
+//       off.style.display = "block";
+//   } else {
+//       off.style.display = "none";
+//   }        
+// }
+
+// // let splashImage = getElementByClassName('splash');
+// document.getElementById('splash').onclick = function() {document.getElementById('splash').style.backgroundImage = 'none';};
